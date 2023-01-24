8fc635e1c3e9814526a2f4e190e9c8593b492af9
Tergel Enkhtur <tergel.e9@gmail.com>
2023-01-23 23:26:16 -0600

    132

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package guru.springframework.spring5recipeapp.controllers;
+package guru.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping({"","/","index"})
-    public String getIndexPage() {
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(){
         return "index";
     }
 }
